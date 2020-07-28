1329e9a3a956049010e1a5341f6590e4a834e2f8
satya-nigam-2020 <68274768+satya-nigam-2020@users.noreply.github.com>
2020-07-28 22:20:26 +0530

    Add files via upload
    28-07-2020(Find Out Questions)

---
+++ ReadExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllDaysFindOutQuestions
+{
+    class ReadExample
+    {
+        static void Main(string[] args)
+        {
+            // use of Read() method 
+            Console.Write(Convert.ToChar(Console.Read()));
+            Console.Write(Convert.ToChar(Console.Read()));
+            Console.Write(Convert.ToChar(Console.Read()));
+        }
+    }
+}
